1423c09c8bea3918400d9ea917ca0c913cad57ce
Sashko Ristovski <sashko.ristovski@yahoo.co.uk>
2022-04-19 20:02:21 +0100

    Refactoring testing

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 // Keeping highscore
 let highscore = 0;
+const displayMessage = function (place, message) {
+  document.querySelector(place).textContent = message;
+};
 
 //Attempt some numnber  and press check
 document.querySelector('.check').addEventListener('click', function () {
@@ -14,7 +17,8 @@ document.querySelector('.check').addEventListener('click', function () {
   // console.log(guess,typeof guess);
   //Stage 1
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number';
+    //document.querySelector('.message').textContent = 'No number';
+    displayMessage('.message', 'No number');
   }
   //Check first if score above 0
   else if (score === 0) {
@@ -22,18 +26,17 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   //Stage 2
   else if (guess === secretNumber) {
-      document.querySelector('.number').textContent = secretNumber; //display correct number
-      document.querySelector('.message').textContent = 'Correct Number';
-      document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-      
+    document.querySelector('.number').textContent = secretNumber; //display correct number
+    //document.querySelector('.message').textContent = 'Correct Number';
+    displayMessage('.message', 'Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
     //Highscores
     //Check highscore and update if it is higher
-    if (score>highscore){
-        document.querySelector('.highscore').textContent = score;
+    if (score > highscore) {
+      document.querySelector('.highscore').textContent = score;
     }
-
-
   }
   //Stage 3
   else if (guess > secretNumber) {
@@ -44,7 +47,7 @@ document.querySelector('.check').addEventListener('click', function () {
   //Stage 4
   else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low !';
-    score --;
+    score--;
     document.querySelector('.score').textContent = score;
   }
 });
@@ -57,9 +60,8 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  score =20;
+  score = 20;
   document.querySelector('.score').textContent = score;
 
   document.querySelector('.guess').value = '';
 });
-
